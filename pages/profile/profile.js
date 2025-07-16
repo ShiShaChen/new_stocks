fb74694de21a016029ef577465afc72b73074385
chenshijun <shijun.chen@dotcunited.com>
2025-07-17 06:54:43 +0800

    v0.3.3

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -504,7 +504,7 @@ Page({
   onAbout() {
     wx.showModal({
       title: '关于应用',
-      content: 'v0.3.0\n\n 样式优化，提高系统稳定性',
+      content: 'v0.3.3\n\n 增加游客预览，提高系统稳定性',
       showCancel: false,
       confirmText: '知道了'
     })
